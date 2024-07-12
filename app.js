df0c64086f44cfd83ca90af631bf169632f1056c
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-07-12 15:15:26 +0000

    gideon's request

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("Amalitech AWS ReStart Program");
+    res.status(200).send("Amalitech AWS ReStart We need our Stipends!!!");
 });
 
 app.listen(8080, () => {
